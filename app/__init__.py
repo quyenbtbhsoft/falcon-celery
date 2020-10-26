34c428d5618d978ea25a216dd38b170caf2fadf7
qbui31 <qbui31@gatech.eduname>
2020-10-26 14:37:07 +0700

    'newparts'

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ import json
 import os
 import falcon
 from app.tasks import fib
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://guest:guest@fibonacci.clxiv.mongodb.net/fibonacci?retryWrites=true&w=majority")
 
 class CheckStatus(object):
 
@@ -9,11 +12,13 @@ class CheckStatus(object):
         taskresult = "Not available" 
         with open("output.txt","r") as file2:
             for line in file2:
-                a = line.split()
-                if a[0] == task_id:
+                a = line[:36]
+                print(a)
+                print(task_id)
+                if a == task_id:
                     taskresult = line[36:len(line)]
                     break
-        result = {'result': taskresult}
+        result = {'result': taskresult[0:len(taskresult)-1]}
         resp.status = falcon.HTTP_200
         resp.body = json.dumps(result)
 
@@ -26,12 +31,41 @@ class CreateTask(object):
         resp.status = falcon.HTTP_200
         result = {
            'task_id': task.id
-        }
-        with open("output.txt","a") as file1:
-            file1.write(task.id + " ")
+            }
         resp.body = json.dumps(result)
+    
 
+class SignUp(object):
+    def on_post(self, req, resp):
+        db = client.test
+        raw_json = req.stream.read()
+        result = json.loads(raw_json,encoding='utf-8')
+        col = db.login
+        myquery = {"ID":result['id']}
+        doc = col.find(myquery)
+        if doc is None:
+            db.login.insert_one(
+            {
+            "ID": result['id'],
+            "password": result['password'],
+            }
+            )
+        else:
+            resp.body = json.dump("Already exist")    
+
+class LogIn(object):
+    def on_get(self,req,resp,id,password):
+        db = client.test
+        col = db.login
+        myquery = {"ID":id}
+        doc = col.find(myquery)
+        if doc["password"] == password:
+            resp.body = json.dumps("Success")
+        else:
+            resp.body = json.dumps("Something wrong") 
 
 application = falcon.API()
 application.add_route('/create', CreateTask())
 application.add_route('/status/{task_id}', CheckStatus())
+application.add_route('/login/{id}/{password}',SignUp())
+application.add_route('/signup', SignUp())
--- app/tasks.py
+++ app/tasks.py
@@ -1,28 +1,42 @@
 from time import sleep
 import os
 import celery
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://guest:guest@fibonacci.clxiv.mongodb.net/fibonacci?retryWrites=true&w=majority")
 
 app = celery.Celery('tasks', broker='amqp://guest:guest@localhost:5672')
 
-@app.task
-def fib(n):
-    results = [0,1]
+@app.task(bind = True)
+def fib(self,n):
+    results = [1,1]
     if n < 0:
         results = []
     elif n == 0:
         results =[0]
     elif n == 1:
-        results =[0, 1]
+        results =[1]
+    elif n == 2:
+        results =[1,1]    
     else:
-        for i in range(n-1):
+        for i in range(n-2):
             results.append(results[len(results)-1] + results[len(results)-2])
+    
+    str1 = "[ " 
+    for i in results:
+        str1 += str(i) + " "
+    str1 += "]" + os.linesep
 
-    with open("output.txt","a") as file1:   	
-        file1.write("[")
-        for i in range(len(results)):
-            file1.write(str(results[i]) + " ")
-        file1.write(str(result[len(result)]))    
-        file1.write("]"+os.linesep)
-        file1.write(os.linesep)
+    with open("output.txt","a+") as file1:   	
+        file1.write(str(self.request.id)+" ")
+        file1.write(str1)
+    
+    db = client.test
+    db.inventory.insert_one(
+        {
+            "ID": "Is",
+            "result": str1,
+        }
+    )
     
     return results  
