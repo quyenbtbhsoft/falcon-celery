4e9102bb4c6707607a54bf256812b5bc0687f601
qbui31 <qbui31@gatech.eduname>
2020-10-28 12:04:09 +0700

    'commandunfinished'

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,15 @@ import os
 import falcon
 from app.tasks import fib
 from pymongo import MongoClient
-from falcon_auth import FalconAuthMiddleware, BasicAuthBackend
-from core.utils import json_serializer
+#from falcon_auth import FalconAuthMiddleware, BasicAuthBackend
+#from core.utils import json_serializer
 
 client = MongoClient("mongodb+srv://guest:guest@fibonacci.clxiv.mongodb.net/fibonacci?retryWrites=true&w=majority")
 
-user_loader = lambda username, password: {'username':username}
-auth_backend = BasicAuthBackend(user_loader)
-auth_middleware = FalconAuthMiddleware(auth_backend)
-application = falcon.API(middleware=[auth_middleware,JSONTranslator()])
+#user_loader = lambda username, password: {'username':username}
+#auth_backend = BasicAuthBackend(user_loader)
+#auth_middleware = FalconAuthMiddleware(auth_backend,exempt_routes=['/exempt'],exempt_method=['HEAD'])
+#application = falcon.API(middleware=[auth_middleware,JSONTranslator()])
 
 class CheckStatus(object):
 
@@ -99,7 +99,7 @@ class JSONTranslator():
         )            
 
 
-
+application = falcon.API()
 
 application.add_route('/create', CreateTask())
 application.add_route('/status/{task_id}', CheckStatus())
